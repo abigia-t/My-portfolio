0058291b13f6c9575a6b4aba0bab9a4fe07eae18
abigia-t <abig80014@gmail.com>
2025-07-06 19:01:37 +0300

    modifying the about page

--- src/Components/Body/Body.jsx
+++ src/Components/Body/Body.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import './Body.css';
-// import Profile_img from '../../assets/Profile_img.jpg';
+import Profile_img from '../../assets/Profile_img.jpg';
 
 const Body = () => {
   return (
     <div className='body'>
-        {/* <img src={Profile_img} alt="Profile" /> */}
+        <img src={Profile_img} alt="Profile" />
         <h1><span>I'm Abigia Tarekegn,</span> fullstack developer. </h1>
         <p> I'm a passionate full-stack developer from Addis Ababa, ready to bring your ideas to life!</p>
    <div className="body-action">
