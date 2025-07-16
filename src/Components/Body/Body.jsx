ae1b048d1166e3ae5cb6223b27c524e932286c1b
abigia-t <abig80014@gmail.com>
2025-07-16 18:12:15 +0300

    resume is aploaded

--- src/Components/Body/Body.jsx
+++ src/Components/Body/Body.jsx
@@ -1,20 +1,22 @@
-import React from 'react'
+import React from 'react';
 import './Body.css';
 import Profile_img from '../../assets/Profile_img.jpg';
+import Resume from '../../assets/ABIGIA TAREKEGN resume.pdf'; // Adjust the path as necessary
 
 const Body = () => {
   return (
     <div className='body'>
         <img src={Profile_img} alt="Profile" />
-        <h1><span>I'm Abigia Tarekegn,</span> fullstack developer. </h1>
+        <h1><span>I'm Abigia Tarekegn,</span> fullstack developer.</h1>
         <p> I'm a passionate full-stack developer from Addis Ababa, ready to bring your ideas to life!</p>
-   <div className="body-action">
-    <div className="body-connect">Connect with me</div>
-    <div className="body-resume">My resume</div>
-
-   </div>
+        <div className="body-action">
+            <div className="body-connect">Connect with me</div>
+            <a href={Resume} target="_blank" rel="noopener noreferrer" className="body-resume">
+              My resume
+            </a>
+        </div>
     </div>
-  )
+  );
 }
 
-export default Body
+export default Body;
